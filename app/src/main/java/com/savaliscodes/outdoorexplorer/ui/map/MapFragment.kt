5ed1d19757a14e2b7dda64a2ef3c4290e023b118
fesavali <felixnzioki99@gmail.com>
2021-11-22 16:19:12 +0300

    add markers and click listeners to map

--- app/src/main/java/com/savaliscodes/outdoorexplorer/ui/map/MapFragment.kt
+++ app/src/main/java/com/savaliscodes/outdoorexplorer/ui/map/MapFragment.kt
@@ -1,14 +1,23 @@
 package com.savaliscodes.outdoorexplorer.ui.map
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.savaliscodes.outdoorexplorer.R
@@ -31,42 +40,58 @@ class MapFragment : Fragment() {
             val bay = LatLng(-1.302221, 36.815502)
             map.moveCamera(CameraUpdateFactory.zoomTo(10f))
             map.moveCamera(CameraUpdateFactory.newLatLng(bay))
+            map.uiSettings.isZoomControlsEnabled = true
+            map.uiSettings.isTiltGesturesEnabled = false
+
 
             mapViewModel.allLocations.observe(viewLifecycleOwner, Observer {
                 for(location in it){
                     val point = LatLng(location.latitude, location.longitude)
-                    map.addMarker(MarkerOptions()
+                    val marker = map.addMarker(MarkerOptions()
                         .position(point)
-                        .title(location.title))
+                        .title(location.title)
+                        .snippet("Hours: ${location.hours}")
+                        .icon(
+                            getBitmapFromVector(R.drawable.ic_star_black_24dp,
+                            R.color.colorAccent)
+                        )
+                        .alpha(.75f))
+                    marker.tag = location.locationId
                 }
             })
+            map.setOnInfoWindowClickListener { marker->
+                val action = MapFragmentDirections.actionNavigationMapToNavigationLocation()
+                action.locationId = marker.tag as Int
+                val navigationController = Navigation.findNavController(requireView())
+                navigationController.navigate(action)
+            }
         }
 
     }
 
-//    private fun getBitmapFromVector(
-//        @DrawableRes vectorResourceId: Int,
-//        @ColorRes colorResourceId: Int
-//    ): BitmapDescriptor {
-//        val vectorDrawable = resources.getDrawable(vectorResourceId, requireContext().theme)
-//            ?: return BitmapDescriptorFactory.defaultMarker()
-//
-//        val bitmap = Bitmap.createBitmap(
-//            vectorDrawable.intrinsicWidth,
-//            vectorDrawable.intrinsicHeight,
-//            Bitmap.Config.ARGB_8888
-//        )
-//
-//        val canvas = Canvas(bitmap)
-//        vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
-//        DrawableCompat.setTint(
-//            vectorDrawable,
-//            ResourcesCompat.getColor(
-//                resources,
-//                colorResourceId, requireContext().theme
-//            )
-//        )
-//        vectorDrawable.draw(canvas)
-//        return BitmapDescriptorFactory.fromBitmap(bitmap)
-//    }
+    private fun getBitmapFromVector(
+        @DrawableRes vectorResourceId: Int,
+        @ColorRes colorResourceId: Int
+    ): BitmapDescriptor {
+        val vectorDrawable = resources.getDrawable(vectorResourceId, requireContext().theme)
+            ?: return BitmapDescriptorFactory.defaultMarker()
+
+        val bitmap = Bitmap.createBitmap(
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+
+        val canvas = Canvas(bitmap)
+        vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
+        DrawableCompat.setTint(
+            vectorDrawable,
+            ResourcesCompat.getColor(
+                resources,
+                colorResourceId, requireContext().theme
+            )
+        )
+        vectorDrawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
 }
