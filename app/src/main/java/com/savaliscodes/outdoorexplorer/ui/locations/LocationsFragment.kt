1977d9285259cdd31fd85a5ef4a4837ca9d6a731
fesavali <felixnzioki99@gmail.com>
2021-11-23 15:54:25 +0300

    Add app location permission check

--- app/src/main/java/com/savaliscodes/outdoorexplorer/ui/locations/LocationsFragment.kt
+++ app/src/main/java/com/savaliscodes/outdoorexplorer/ui/locations/LocationsFragment.kt
@@ -1,6 +1,8 @@
 package com.savaliscodes.outdoorexplorer.ui.locations
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.style.EasyEditSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,8 +10,13 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import com.google.android.gms.location.LocationServices
+import com.google.android.material.snackbar.Snackbar
 import com.savaliscodes.outdoorexplorer.R
+import com.savaliscodes.outdoorexplorer.data.Location
 import kotlinx.android.synthetic.main.fragment_locations.*
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
 
 class LocationsFragment : Fragment(), LocationsAdapter.OnClickListener {
     private lateinit var adapter: LocationsAdapter
@@ -41,6 +48,47 @@ class LocationsFragment : Fragment(), LocationsAdapter.OnClickListener {
                     })
             }
         }
+        getCurrentLocation()
+    }
+
+    @SuppressLint("MissingPermission")
+    @AfterPermissionGranted(RC_LOCATION)
+    private fun getCurrentLocation() {
+        if(EasyPermissions.hasPermissions(requireContext(),
+            android.Manifest.permission.ACCESS_FINE_LOCATION)){
+            val fusedLocationProviderClient =
+                LocationServices.getFusedLocationProviderClient(requireActivity())
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener { location : android.location.Location? ->
+                if(location != null){
+                    adapter.setCurrentLocation(location)
+                }
+            }
+        }else{
+            Snackbar.make(
+                requireView(),
+                getString(R.string.snackbar),
+                Snackbar.LENGTH_INDEFINITE
+            ).setAction(R.string.ok){
+                EasyPermissions.requestPermissions(
+                    this,
+                    getString(R.string.rationale_text),
+                    RC_LOCATION,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            }.show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(
+            requestCode, permissions,
+            grantResults, this
+        )
     }
 
     override fun onClick(id: Int) {
@@ -49,5 +97,8 @@ class LocationsFragment : Fragment(), LocationsAdapter.OnClickListener {
         action.locationId = id
         val navController = Navigation.findNavController(requireView())
         navController.navigate(action)
+    }
+    companion object{
+        const val RC_LOCATION = 1
     }
 }
